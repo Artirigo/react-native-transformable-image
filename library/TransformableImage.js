f6010285ca40585bec4b5ef80862d1a67c342227
Andre Kovac <me@andrekovac.com>
2017-03-01 18:18:23 -0300

    Add onSingleTap and onDoubleTap props and pass on to ViewTransformer as props
    Complementary change to commits in react-native-view-transformer of addition of
    these callbacks.

--- library/TransformableImage.js
+++ library/TransformableImage.js
@@ -22,7 +22,21 @@ export default class TransformableImage extends Component {
     enableTransform: PropTypes.bool,
     enableScale: PropTypes.bool,
     enableTranslate: PropTypes.bool,
+    /**
+     * Callback after a single tap (includes first and second tap of double-tap)
+     */
+    onSingleTap: PropTypes.func,
+    /**
+     * Callback after a single tap which is not part of a double-tap
+     */
     onSingleTapConfirmed: PropTypes.func,
+    /**
+     * Callback on a double tap (if supplied, it overwrites the default scaling behavior)
+     *
+     * The first two arguments of the callback are the x- and y-coordinates of the
+     * finger while tapping, respectively.
+     */
+    onDoubleTap: PropTypes.func,
     onTransformGestureReleased: PropTypes.func,
     onViewTransformed: PropTypes.func
   };
@@ -32,7 +46,9 @@ export default class TransformableImage extends Component {
     enableTransform: true,
     enableScale: true,
     enableTranslate: true,
+    onSingleTap: undefined,
     onSingleTapConfirmed: undefined,
+    onDoubleTap: undefined,
     onTransformGestureReleased: undefined,
     onViewTransformed: undefined,
   };
@@ -98,7 +114,9 @@ export default class TransformableImage extends Component {
         enableResistance={true}
         onTransformGestureReleased={this.props.onTransformGestureReleased}
         onViewTransformed={this.props.onViewTransformed}
+        onSingleTap={this.props.onSingleTap}
         onSingleTapConfirmed={this.props.onSingleTapConfirmed}
+        onDoubleTap={this.props.onDoubleTap}
         maxScale={maxScale}
         contentAspectRatio={contentAspectRatio}
         onLayout={this.onLayout.bind(this)}
