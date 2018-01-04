bb6c5ea72f5e9f2d2e96774a20c935187c0fb864
Christian Ost <christian@christianost.de>
2018-01-04 12:57:50 +0100

    Update code to use prop-types-package instead of deprecated ones provided by React (#1)
    * add gitignore
    
    * add lockfile and prop types dependency
    
    * switch to external prop types package
    
    * cross reference the correct dependency
    
    * refer to master branch instead of feature branch

--- library/TransformableImage.js
+++ library/TransformableImage.js
@@ -1,14 +1,14 @@
 'use strict';
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { Image } from 'react-native';
+import { PropTypes } from 'prop-types';
 
 import ViewTransformer from 'react-native-view-transformer';
 
 let DEV = false;
 
 export default class TransformableImage extends Component {
-
   static enableDebug() {
     DEV = true;
   }
